3444cd5cbefe428f3b07cd700286f8bd6b47894a
Thoufeeque <thoufeeque.rafique@gmail.com>
2025-06-10 15:26:57 +0200

    added WebDriverManager

--- src/test/java/full_framework/base/BaseTest.java
+++ src/test/java/full_framework/base/BaseTest.java
@@ -2,10 +2,12 @@
 package full_framework.base;
 
 import full_framework.utils.ConfigLoader;
-import org.apache.logging.log4j.LogManager; // Import LogManager
-import org.apache.logging.log4j.Logger;   // Import Logger
+import io.github.bonigarcia.wdm.WebDriverManager; // Import WebDriverManager
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
 
@@ -14,7 +16,6 @@ import java.time.Duration;
 public class BaseTest {
 
     public WebDriver driver;
-    // Initialize a logger for the BaseTest class
     private static final Logger logger = LogManager.getLogger(BaseTest.class);
 
     @BeforeSuite
@@ -28,25 +29,38 @@ public class BaseTest {
     public void setup() {
         String browser = ConfigLoader.getStringProperty("browser");
         int implicitWaitSeconds = Integer.parseInt(ConfigLoader.getStringProperty("implicit.wait.seconds"));
-        int explicitWaitSeconds = Integer.parseInt(ConfigLoader.getStringProperty("explicit.wait.seconds")); // Log explicit wait too
+        int explicitWaitSeconds = Integer.parseInt(ConfigLoader.getStringProperty("explicit.wait.seconds"));
 
-        logger.info("Initializing WebDriver for browser: {}", browser);
+        logger.info("Initializing WebDriver for browser: " + browser);
 
         switch (browser.toLowerCase()) {
             case "chrome":
+                // Use WebDriverManager to setup ChromeDriver
+                WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
+                logger.info("ChromeDriver initialized via WebDriverManager.");
                 break;
             case "firefox":
+                // Use WebDriverManager to setup FirefoxDriver
+                WebDriverManager.firefoxdriver().setup();
                 driver = new FirefoxDriver();
+                logger.info("FirefoxDriver initialized via WebDriverManager.");
                 break;
+            case "edge":
+                // Use WebDriverManager to setup EdgeDriver
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
+                logger.info("EdgeDriver initialized via WebDriverManager.");
+                break;
+            // Add more browsers as needed
             default:
-                logger.error("Browser '{}' is not supported. Please check config.properties.", browser);
+                logger.error("Browser '" + browser + "' is not supported. Please check config.properties.");
                 throw new IllegalArgumentException("Browser '" + browser + "' is not supported.");
         }
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWaitSeconds));
-        logger.info("WebDriver initialized. Maximize window, implicit wait: {}s, explicit wait default: {}s.", implicitWaitSeconds, explicitWaitSeconds);
+        logger.info("WebDriver initialized. Maximize window, implicit wait: " + implicitWaitSeconds + "s, explicit wait default: " + explicitWaitSeconds + "s.");
     }
 
     @AfterMethod
